46d8f87234b20866cd4e2b40f6e91cc6e268ff59
derberg <lpgornicki@gmail.com>
2020-09-07 18:19:45 +0200

    chore: fix pre commit

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,19 @@ const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 const eventName = process.env.GITHUB_EVENT_NAME;
 
 async function run() {
-  core.debug(eventName, eventPayload);
+  switch (eventName) {
+  case 'issues':
+    console.log('event payload', eventPayload);  
+    break;
+  case 'issue_comment':
+    break;
+  case 'pull_request':
+    break;
+  default:
+    break;
+  }
+  
+  //core.debug(eventName, eventPayload);
   core.setOutput('sentiment', 'TODO pass sentiment info here');
   core.setOutput('source', 'TODO pass link to the location where is the comment');
 }
