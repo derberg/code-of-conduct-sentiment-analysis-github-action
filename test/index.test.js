706764e436ba8ac60e689bdbae380fffdcd02b55
derberg <lpgornicki@gmail.com>
2020-09-15 16:35:02 +0200

    test: remove not needed comment

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,6 @@ describe('processingEvent()', () => {
   it('analyzis should log proper message to user that only given events are supported', async () => {
     console.log = jest.fn();
     await processingEvent('newuser', {});
-    // The first argument of the first call to the function was 'hello'
     expect(console.log).toHaveBeenCalledWith('Only the following events are supported by the action: issues, issue_comment, pull_request_review_comment, pull_request, pull_request_review');
   });
 });
